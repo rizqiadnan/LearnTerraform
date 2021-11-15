5d87f65ad19adfe29844037b15f6a18fd76805df
rizqiadnan <rizkyadnan23@gmail.com>
2021-11-15 12:15:11 +0700

    config main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 3.27"
+		}
+	}
+}
+
+provider "aws" {
+	region = "ap-southeast-1"
+}
+
+resource "aws_instances" "tfInstance" {
+	ami					= "ami-0907c2c44ea451f84"
+	instance_type		= "t2.micro"		
+}
+
+tags = {
+	Name = "TfInstance"
+	ManagedBy = "Terraform"
+}
